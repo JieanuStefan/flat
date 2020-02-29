a8422e41f82e89cf75645eb2b40544e88301428b
StefanJ00 <stefan.jieanu00@e-uvt.com>
2020-02-29 02:13:54 +0200

    Generating the first generation of words

--- language generator/lg.py
+++ language generator/lg.py
@@ -1,15 +1,50 @@
 # $ = empty word
+# In the file the 'variables' are in the folloing order
+# Non-terminals
+# Terminals
+# Starting point
+# Production rules
 
 non_terminals = []
 terminals = []
 starting_point = []
-production_rules = []
+production_rules = {}
 
+language_tree = []
 
-with open('grammar.txt', 'r') as f:
-    non_terminals = f.readline()
-    terminals = f.readline()
+with open('grammar2.txt', 'r') as f:
+    non_terminals = f.readline().strip().split(' ')
+    terminals = f.readline().strip().split(' ')
     starting_point = f.readline()
     # read the production rules into a list and then make a dict from it
-    production_rules = f.readline()
+    temp = f.readline().split(',')
+    for rule in temp:
+        # if the rule is an empty strin, skip it
+        if len(rule) == 0:
+            continue
 
+        p = rule.split(' ')
+        production_rules[p[0]] = []
+        for d in p[1:]:
+            production_rules[p[0]].append(d)
+
+def generate_tree(ex, tree):
+    found_non_terminal = False
+    for non_term in non_terminals:
+        if non_term in ex:
+            found_non_terminal = True
+            break
+
+    if found_non_terminal:
+        tree = []
+        for i in range(0, len(ex)):
+            if ex[i] in non_terminals:
+                for rule in production_rules[ex[i]]:
+                    if len(rule) % 2 == 0:
+                        print(ex[:i] + rule + ex[(i-1+len(rule)):])
+                    else:
+                        print(ex[:i] + rule + ex[(i+len(rule)):])
+    else:
+        return ex
+
+print(generate_tree(starting_point, []))
